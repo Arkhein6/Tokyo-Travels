8dd1ecb450be7660e48aa90b894432c6c41befb7
RedStar6 <richmondnkrumah04@gmail.com>
2023-10-14 00:13:39 +0000

    fixed color changes

--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -1,16 +1,16 @@
 import { NavBar, BottomNav } from "../Components";
+
 const Header = () => {
 	return (
-		<div className="flex justify-center content-center w-screen h-screen bg-gray-500">
-			<div className="flex flex-col w-[80%] h-full bg-yellow-200">
+		<div className="flex justify-center content-center w-screen h-screen HEADER">
+			<div className="flex flex-col w-[80%] h-full">
 				<header>
 					<NavBar />
 				</header>
-				<div className="flex grow bg-green-300 pl-5 items-center">
+				<div className="flex grow pl-5 items-center">
 					<h1 className=" text-white font-bold text-8xl ">VISIT <br></br> TOKYO</h1>
 				</div>
 				<BottomNav />
-				
 			</div>
 		</div>
 	);
